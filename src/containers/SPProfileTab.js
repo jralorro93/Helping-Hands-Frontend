2ff2c17da5451c30e080f1fdbf2cabb4a90eb85a
github username <github email address>
2019-07-16 16:33:58 -0400

    dsa

---
+++ src/containers/SPProfileTab.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class SPProfileTab extends Component {
+  render() {
+    console.log('this is currentUser: ', this.props.currentUser)
+    let fullName = this.props.currentUser.first_name + ' ' + this.props.currentUser.last_name
+    return (
+      <div>
+        <h1>{fullName}</h1>
+        
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {currentUser: state.login.user }
+}
+
+export default connect(mapStateToProps)(SPProfileTab)
