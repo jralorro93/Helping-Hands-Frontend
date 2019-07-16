457f31aeb0dd46a6d1c92af38b2bedd49c792ca6
github username <github email address>
2019-07-16 17:05:52 -0400

    Added profile

--- src/containers/SPProfileTab.js
+++ src/containers/SPProfileTab.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Divider } from 'semantic-ui-react'
 
 class SPProfileTab extends Component {
+
   render() {
-    console.log('this is currentUser: ', this.props.currentUser)
+    console.log('this is currentUser: ', this.props.currentUser.appointments.length)
     let fullName = this.props.currentUser.first_name + ' ' + this.props.currentUser.last_name
     return (
       <div>
-        <h1>{fullName}</h1>
-<<<<<<< HEAD
-        
-=======
->>>>>>> jep
+        <div id='personalInfo'>
+          <h1>{fullName}</h1>
+          <h3>{this.props.currentUser.email}</h3>
+          <Divider />
+          <h1>Profession: </h1>
+          <h3>{this.props.currentUser.service.job}</h3>
+          <p>{this.props.currentUser.service.description}</p>
+          <p>{this.props.currentUser.service.availability}</p>
+          <Divider />
+          <h1>Bookings: </h1>
+          <h4>Number of bookings: {this.props.currentUser.appointments.length}</h4>
+        </div>
       </div>
     )
   }
